10bd037beaac8dadc6bea774ce4f908554b74d2e
Marcin Lenkowski <marcin@lenkowski.net>
2023-06-29 17:34:41 +0200

    First Commit

---
+++ content.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+    <style>
+        body {
+            font-family: Helvetica, Arial, sans-serif;
+            font-size: 16px;
+            margin: 50px 20%;
+        }
+
+        textarea {
+            border: 1px solid #ccc;
+            font-family: Georgia, serif;
+            width: 100%;
+            font-size: 16px;
+        }
+        </style>
+</head>
+<body>
+    <h1>Analiza dziennika:</h1>
+    <form method="post" action="/">
+        <textarea name="inputFromDairy" rows="10" cols="100"><?php echo $inputFromDairy; ?></textarea>
+        <button type="submit">Wyślij</button>
+    </form>
+
+    <?php if($responseData): ?>
+        <h1>Wynik:</h1>
+        <pre><?php echo ($responseData?->choices[0]?->message?->content); ?></pre>
+    <?php endif; ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+
+require_once('vendor/autoload.php');
+
+const BASE_URL = 'https://api.openai.com/v1';
+const API_KEY = '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';
+
+$inputFromDairy = $_POST['inputFromDairy'] ?? null;
+
+$body = [
+    'model' => 'gpt-3.5-turbo',
+    'messages' => [["role" => "user", "content" => 'Na podstawie wpisu z dziennika: `' . $inputFromDairy . '`. Zadaj 3 pytania do zastanowienia się nad tymi zagadnieniami, które mogą je pogłębić.']]
+];
+$header = ['Authorization' => 'Bearer ' . API_KEY, 'Content-Type' => 'application/json'];
+
+$client = new Client([
+    'base_uri' => BASE_URL,
+]);
+$request = new Request(
+    'POST',
+    BASE_URL . '/chat/completions',
+    $header,
+    json_encode($body)
+);
+
+$responseData = null;
+if($inputFromDairy) {
+    $response = $client->send($request);
+    $responseData = json_decode($response->getBody()->getContents());
+}
+
+$inputFromDairy ??= "Stworzyłem na swojej stronie ofertę indywidualnych szkoleń programistycznych, tak by wszystko było do kupienia.
+
+Zrobiłem też trochę zmian na stronie i wprowadziłem slajdy.
+
+Pod koniec dnia stworzyłem scenariusz, ale obawiam się, że nie jest wystarczająco idealny.
+
+Muszę się nauczyć robienia lepszych i ciekawszych filmów. Również takich, które będą prostsze.
+
+Montaż zajmuje dużo czasu. Dobrze napisane scenariusze mogą mi pomóc skrócić ten czas drastycznie i zrobić materiały, które bardziej się spodobają innym ludziom.
+
+Vega robił plany oddziałujące odpowiednio na widza, różnymi bodźcami. Projektowanie emocji jest kluczowe!";
+
+
+include 'content.php';
